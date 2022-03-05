76e425b5632a8c9d9fb2c1b5c299d126757a6b3a
Okky Maheswara <okkymahes@gmail.com>
2022-03-05 19:40:55 +0800

    data fetching getstaticprops

--- pages/users/index.js
+++ pages/users/index.js
@@ -1,9 +1,29 @@
 import Layout from "../../components/Layout";
 
-export default function User() {
+export default function User(props) {
+  const { dataUsers } = props;
+  // console.log(dataUsers);
   return (
     <Layout pageTitle="Users Page">
-      <p>User Page</p>
+      <ol>
+        {dataUsers.map((user) => (
+          <li key={user.id}>
+            Name : {user.name}<br />
+            Email : {user.email}<br />
+          </li>
+        ))}
+      </ol>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const dataUsers = await res.json();
+
+  return {
+    props: {
+      dataUsers,
+    },
+  }
 }
