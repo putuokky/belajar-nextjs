dba339942a86d52c895a622d04cebbcff55c87b1
Okky Maheswara <okkymahes@gmail.com>
2022-03-05 18:26:41 +0800

    optimalisasi image

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
-import Head from 'next/head';
+import Image from 'next/image';
 import Layout from '../components/Layout';
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <Layout pageTitle="Home Page">
+      <Image src='/kucing.jpg' width={200} height={300} alt='kucing' />
       <h1>Welcome Okky Maheswara</h1>
     </Layout>
   )
