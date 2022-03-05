211623e43c62c1fa154cd3382b11b154bc002992
Okky Maheswara <okkymahes@gmail.com>
2022-03-05 18:08:17 +0800

    buat metadata

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,13 +1,20 @@
+import Head from 'next/head';
 import Footer from "../Footer";
 import Header from "../Header";
 
 export default function Layout(props) {
-  const { children } = props;
+  const { children, pageTitle } = props;
   return (
-    <div>
-      <Header />
-      <div>{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Head>
+        <title>{pageTitle} | Belajar NextJS</title>
+        <meta name="description" content="Belajar NextJS" />
+      </Head>
+      <div>
+        <Header />
+        <div>{children}</div>
+        <Footer />
+      </div>
+    </>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import Head from 'next/head';
 import Layout from '../components/Layout';
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <Layout>
+    <Layout pageTitle="Home Page">
       <h1>Welcome Okky Maheswara</h1>
     </Layout>
   )
