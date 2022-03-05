d3cbc5f18401f757949451ed0396413ff957e844
Okky Maheswara <okkymahes@gmail.com>
2022-03-05 17:49:50 +0800

    auto redirect

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,14 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react"
+
 export default function Custom404() {
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/');
+    }, 2000)
+  }, [])
   return (
     <div>
       <h1 className="title-not-found">Ooooopss....</h1>
